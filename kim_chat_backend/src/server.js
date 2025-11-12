d7aa62ad363a3f3839d03d7e4f994e58b9cc6f86
KOUSTUBH BADSHAH <137117503+frustateduser@users.noreply.github.com>
2025-11-13 02:18:24 +0530

    Remove CSRF middleware from server.js
    Removed CSRF middleware from server configuration.

--- kim_chat_backend/src/server.js
+++ kim_chat_backend/src/server.js
@@ -10,7 +10,6 @@ import ChatSocket from "./socket/chatSocket.js";
 import requestLogger from "./middleware/requestLogger.js";
 import cookieParser from "cookie-parser";
 import { connectRedis } from "./utils/redisClient.js";
-import { csrf } from "lusca";
 
 connectDataBase();
 const redisClient = await connectRedis();
@@ -26,7 +25,6 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use(csrf());
 app.use(requestLogger); // to remove only for debug incomming request's
 
 app.use("/api/auth", authRoutes);
