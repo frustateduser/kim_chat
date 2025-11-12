4addf8242ea015fd3cde09ab5b171166c954b79e
frustateduser <koustubhbadshah07@outlook.com>
2025-11-13 03:04:42 +0530

    Fix import path for SearchBar component in Navbar

--- kim_chat_frontend/src/components/Navbar.jsx
+++ kim_chat_frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '@context/useAuth';
-import SearchBar from '@components/Searchbar';
+import SearchBar from '@components/SearchBar';
 
 function Navbar({ onUserFound }) {
   const { userLogout } = useAuth();
